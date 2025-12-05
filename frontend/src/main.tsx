91a5788787e4f4a96eca3a47a6cb28a5e78b18a6
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2025-12-06 03:13:57 +0530

    ui-upgrade

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './components/theme-provider.tsx'
+
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
