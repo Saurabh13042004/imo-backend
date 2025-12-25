c21d948b2131cdb89f3ae87aa98c3f5ce63c1ed3
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2025-12-25 22:17:33 +0530

    feat:ms-clarity

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,14 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from './components/theme-provider.tsx'
-
+import Clarity from '@microsoft/clarity';
 
 const queryClient = new QueryClient()
+
+const clarityProjectId = import.meta.env.VITE_CLARITY_PROJECT_ID;
+if (clarityProjectId) {
+  Clarity.init(clarityProjectId);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
